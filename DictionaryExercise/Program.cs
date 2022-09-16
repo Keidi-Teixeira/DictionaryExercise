9057c4cb1cd05ba53caf0735dd2b625a5f557bec
Keidi-Teixeira <keidifofa@gmail.com>
2022-09-16 10:31:22 -0300

    Adicionar arquivos de projeto.

---
+++ DictionaryExercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DictionaryExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indique o caminho do arquivo: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                        Thread.Sleep(500);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu durante a execução.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
